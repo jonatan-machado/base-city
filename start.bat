d63086565e64a7bfc443304e38e3ea32dbcebb86
Jonatan Machado <jonatanmachado35@gmail.com>
2022-05-15 16:58:22 +0000

    first commit

---
+++ start.bat
@@ -0,0 +1,12 @@
+@echo off
+
+echo ===-------------------------------===
+echo     ZIRIX V3 (3.2.0)
+echo     Developed by: ZIRAFLIX
+echo     Discord: discord.gg/kYFy8JwVfd
+echo     Contact: contato@ziraflix.com
+echo ===-------------------------------===
+
+pause
+start ..\artifacts\FXServer.exe +exec config/config.cfg +set onesync on +set onesync_population false +set sv_enforceGameBuild tuner
+exit
