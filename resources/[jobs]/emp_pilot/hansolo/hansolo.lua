d4286a62052be6ed4090847b6986ec351c313fa2
Jonatan Machado <jonatanmachado35@gmail.com>
2022-05-15 17:00:14 +0000

    first commit

---
+++ resources/[jobs]/emp_pilot/hansolo/hansolo.lua
@@ -0,0 +1,358 @@
+local Tunnel = module("vrp","lib/Tunnel")
+local Proxy = module("vrp","lib/Proxy")
+vRP = Proxy.getInterface("vRP")
+
+vSERVER = Tunnel.getInterface("vrp_garages")
+
+--[ CONNECTION ]----------------------------------------------------------------------------------------------------------------
+
+emp16 = Tunnel.getInterface("emp_pilot")
+
+--[ VARIABLES ]-----------------------------------------------------------------------------------------------------------------
+
+local emservico = false
+local menuactive = false
+local wood = false
+local gas = false
+local criado = false
+local servehicle = nil
+local entrega = 0
+local pay = 0
+
+--[ FUNCTION ]------------------------------------------------------------------------------------------------------------------
+
+function CalculateTimeToDisplay6()
+	hour = GetClockHours()
+	if hour <= 9 then
+		hour = "0" .. hour
+	end
+end
+
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- FUNCTION
+-----------------------------------------------------------------------------------------------------------------------------------------
+
+function ToggleActionMenu()
+	menuactive = not menuactive
+	if menuactive then
+		SetNuiFocus(true,true)
+		SendNUIMessage({ showmenu = true })
+	else
+		SetNuiFocus(false)
+		SendNUIMessage({ hidemenu = true })
+	end
+end
+
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- BUTTON
+-----------------------------------------------------------------------------------------------------------------------------------------
+
+RegisterNUICallback("ButtonClick",function(data,cb)	
+	if data == "airport-1" then
+		if emservico == true then
+			TriggerEvent("Notify","importante","Voce ja esta em serviço.",8000)
+		else
+			for k,v in pairs(configpilot.spawn) do
+				if k == 1 then
+					TriggerEvent("Notify","sucesso","Avião liberado para decolagem.")
+					spawnVehicle(configpilot.plane,v.x,v.y,v.z)
+					servehicle = GetEntityModel(configpilot.plane)
+					emservico = true
+					gas = true
+					entrega = 1
+					pay = configpilot.routeshamal[entrega].pay
+					ToggleActionMenu()
+				end
+			end
+		end
+	elseif data == "airport-2" then
+		if emservico == true then
+			TriggerEvent("Notify","importante","Voce ja esta em serviço.",8000)
+		else
+			for k,v in pairs(configpilot.spawn) do
+				if k == 2 then 
+					TriggerEvent("Notify","sucesso","Avião liberado para decolagem.")
+					spawnVehicle(configpilot.plane,v.x,v.y,v.z)
+					servehicle = GetEntityModel(configpilot.plane)
+					emservico = true
+					gas = true
+					entrega = 2
+					pay = configpilot.routeshamal[entrega].pay
+					ToggleActionMenu()
+				end
+			end
+		end
+	elseif data == "airport-3" then
+		if emservico == true then
+			TriggerEvent("Notify","importante","Voce ja esta em serviço.",8000)
+		else
+			for k,v in pairs(configpilot.spawn) do
+				if k == 3 then
+					spawnVehicle(configpilot.plane,v.x,v.y,v.z)
+					TriggerEvent("Notify","sucesso","Avião liberado para decolagem.")
+					servehicle = GetEntityModel(configpilot.plane)
+					emservico = true
+					gas = true
+					entrega = 3
+					pay = configpilot.routeshamal[entrega].pay
+					ToggleActionMenu()
+				end
+			end
+		end
+	elseif data == "airport-01" then
+		if emservico == true then
+			TriggerEvent("Notify","importante","Voce ja esta em serviço.",8000)
+		else
+			for k,v in pairs(configpilot.spawn) do
+				if k == 4 then
+					spawnVehicle(configpilot.plane1,v.x,v.y,v.z)
+					TriggerEvent("Notify","sucesso","Avião liberado para decolagem.")
+					servehicle = GetEntityModel(configpilot.plane1)
+					emservico = true
+					wood = true
+					entrega = 1
+					pay = configpilot.routemiljet[entrega].pay
+					ToggleActionMenu()
+				end
+			end
+		end
+	elseif data == "airport-02" then
+		if emservico == true then
+			TriggerEvent("Notify","importante","Voce ja esta em serviço.",8000)
+		else
+			for k,v in pairs(configpilot.spawn) do
+				if k == 5 then
+					spawnVehicle(configpilot.plane1,v.x,v.y,v.z)
+					TriggerEvent("Notify","sucesso","Avião liberado para decolagem.")
+					servehicle = GetEntityModel(configpilot.plane1)
+					emservico = true
+					wood = true
+					entrega = 2
+					pay = configpilot.routemiljet[entrega].pay
+					ToggleActionMenu()
+				end
+			end
+		end
+	elseif data == "airport-03" then
+		if emservico == true then
+			TriggerEvent("Notify","importante","Voce ja esta em serviço.",8000)
+		else
+			for k,v in pairs(configpilot.spawn) do
+				if k == 6 then
+					spawnVehicle(configpilot.plane1,v.x,v.y,v.z)
+					TriggerEvent("Notify","sucesso","Avião liberado para decolagem.")
+					servehicle = GetEntityModel(configpilot.plane1)
+					emservico = true
+					wood = true
+					entrega = 3
+					pay = configpilot.routemiljet[entrega].pay
+					ToggleActionMenu()
+				end
+			end
+		end
+	end
+	if data == "fechar" then
+		ToggleActionMenu()	
+	end
+end)
+
+--[ THREAD ]-----------------------------------------------------------------------------------------------------------------
+
+Citizen.CreateThread(function()
+	while true do
+		local idle = 1000
+		if not emservico then
+			for _,v in pairs(configpilot.start) do
+				local ped = PlayerPedId()
+				local x,y,z = table.unpack(GetEntityCoords(ped))
+				local bowz,cdz = GetGroundZFor_3dCoord(v.x,v.y,v.z)
+				local distance = GetDistanceBetweenCoords(v.x,v.y,cdz,x,y,z,true)
+				local lastVehicle = GetEntityModel(GetPlayersLastVehicle())
+					if distance <= 3 then
+						idle = 5
+						DrawMarker(23,v.x,v.y,v.z-0.99,0, 0, 0, 0, 0, 0, 0.7, 0.7, 0.5, 136, 96, 240, 180, 0, 0, 0, 0)
+						if distance <= 1.2 then
+							drawTxt("PRESSIONE  ~p~E~w~  PARA INICIAR O SERVIÇO",4,0.5,0.92,0.35,255,255,255,180)
+							if IsControlJustPressed(1,38) then
+								CalculateTimeToDisplay6()
+                if parseInt(hour) >= 06 and parseInt(hour) <= 20 then
+									ToggleActionMenu()
+								else
+									TriggerEvent("Notify","importante","Funcionamento é das <b>06:00</b> as <b>20:00</b>.",8000)
+								end
+							end
+						end
+					end
+				end
+			end
+		Citizen.Wait(idle)
+	end
+end)
+
+--[ DELIVERY ]-----------------------------------------------------------------------------------------------------------------
+Citizen.CreateThread(function()
+	while true do
+		local idle = 1000
+		local ped = PlayerPedId()
+		if gas then
+			if emservico then
+				local x,y,z = table.unpack(GetEntityCoords(ped))
+				local bowz,cdz = GetGroundZFor_3dCoord(configpilot.routeshamal[entrega].x, configpilot.routeshamal[entrega].y, configpilot.routeshamal[entrega].z)
+				local distance = GetDistanceBetweenCoords(GetEntityCoords(ped),configpilot.routeshamal[entrega].x,configpilot.routeshamal[entrega].y,configpilot.routeshamal[entrega].z,true)
+				if criado == false then
+					criandoblip(configpilot.routeshamal[entrega].x, configpilot.routeshamal[entrega].y, configpilot.routeshamal[entrega].z)
+					criado = true
+				end
+				if distance <= 100.0 then
+					idle = 5
+					DrawMarker(23,configpilot.routeshamal[entrega].x, configpilot.routeshamal[entrega].y, configpilot.routeshamal[entrega].z-0.96,0, 0, 0, 0, 0, 0, 10.0, 10.0, 1.0, 136, 96, 240, 180, 0, 0, 0, 0)
+					if distance <= 10.0 then
+						if IsControlJustPressed(0,38) then
+							--if not IsPedInAnyVehicle(ped) then
+								local vehicle = getVehicleInDirection(GetEntityCoords(PlayerPedId()),GetOffsetFromEntityInWorldCoords(PlayerPedId(),0.0,5.0,0.0))
+								if GetEntityModel(vehicle) == servehicle then
+									vSERVER.deleteVehicles(vehicle)
+									emp16.checkPaymentShamal(pay)
+									RemoveBlip(blip)
+									emservico = false
+									criado = false	
+									gas = false
+								end
+							--else	
+							--	TriggerEvent("Notify","importante","Saia do <b>Avião</b>.",8000)					
+							--end
+						end
+					end
+				end
+			end
+		end
+		Citizen.Wait(idle)
+	end
+end)
+
+Citizen.CreateThread(function()
+	while true do
+		local idle = 1000
+		local ped = PlayerPedId()
+		if wood then
+			if emservico then
+				local x,y,z = table.unpack(GetEntityCoords(ped))
+				local bowz,cdz = GetGroundZFor_3dCoord(configpilot.routemiljet[entrega].x, configpilot.routemiljet[entrega].y, configpilot.routemiljet[entrega].z)
+				local distance = GetDistanceBetweenCoords(configpilot.routemiljet[entrega].x, configpilot.routemiljet[entrega].y,cdz,x,y,false)
+				if criado == false then
+					criandoblip(configpilot.routemiljet[entrega].x, configpilot.routemiljet[entrega].y, configpilot.routemiljet[entrega].z)
+					criado = true
+				end
+				if distance <= 100.0 then
+				idle = 5
+				DrawMarker(23,configpilot.routemiljet[entrega].x, configpilot.routemiljet[entrega].y, configpilot.routemiljet[entrega].z-0.96,0, 0, 0, 0, 0, 0, 10.0, 10.0, 1.0, 136, 96, 240, 180, 0, 0, 0, 0)
+					if distance <= 10.0 then						
+						if IsControlJustPressed(0,38) then
+							--if IsPedInAnyVehicle(ped) then
+								local vehicle = GetEntityModel(GetPlayersLastVehicle())
+								if GetEntityModel(vehicle) == servehicle then
+									vSERVER.deleteVehicles(vehicle)
+									emp16.checkPaymentMiljet(pay)
+									RemoveBlip(blip)
+									emservico = false
+									criado = false	
+									wood = false
+								end
+							--else	
+							--	TriggerEvent("Notify","importante","Entre no <b>Avião</b> para terminar o serviço.",8000)					
+							--end
+						end
+					end
+				end
+			end
+		end
+		Citizen.Wait(idle)
+	end
+end)
+
+
+--[ CANCEL | THREAD ]--------------------------------------------------------------------------------------------------------------------
+
+Citizen.CreateThread(function()
+	while true do
+		local idle=1000
+		if emservico then
+			idle=5
+			if IsControlJustPressed(0,168) then
+					emservico = false
+					RemoveBlip(blip)
+					TriggerEvent("Notify","importante","Você cancelou o serviço.",8000)
+					wood = false
+					gas = false
+					criado = false
+			end
+		end
+		Citizen.Wait(idle)
+	end
+end)
+
+--[ FUNCTION ]--------------------------------------------------------------------------------------------------------
+
+function drawTxt(text,font,x,y,scale,r,g,b,a)
+	SetTextFont(font)
+	SetTextScale(scale,scale)
+	SetTextColour(r,g,b,a)
+	SetTextOutline()
+	SetTextCentre(1)
+	SetTextEntry("STRING")
+	AddTextComponentString(text)
+	DrawText(x,y)
+end
+
+function criandoblip(x,y,z)
+	blip = AddBlipForCoord(x,y,z)
+	SetBlipSprite(blip,1)
+	SetBlipColour(blip,27)
+	SetBlipScale(blip,0.4)
+	SetBlipAsShortRange(blip,false)
+	SetBlipRoute(blip,true)
+	BeginTextCommandSetBlipName("STRING")
+	AddTextComponentString("Plano de Voo")
+	EndTextCommandSetBlipName(blip)
+	criado = true
+end
+
+function spawnVehicle(name,x,y,z)
+	local mhash = GetHashKey(name)
+	while not HasModelLoaded(mhash) do
+		RequestModel(mhash)
+		Citizen.Wait(10)
+	end
+	
+	if HasModelLoaded(mhash) then
+		local nveh = CreateVehicle(mhash,x,y,z,147.22,true,false)
+		local netveh = VehToNet(nveh)
+		local id = NetworkGetNetworkIdFromEntity(nveh)
+
+		NetworkRegisterEntityAsNetworked(nveh)
+		while not NetworkGetEntityIsNetworked(nveh) do
+			NetworkRegisterEntityAsNetworked(nveh)
+			Citizen.Wait(1)
+		end
+
+		if NetworkDoesNetworkIdExist(netveh) then
+			SetEntitySomething(nveh,true)
+			if NetworkGetEntityIsNetworked(nveh) then
+				SetNetworkIdExistsOnAllMachines(netveh,true)
+			end
+		end
+
+		SetNetworkIdCanMigrate(id,true)
+		SetVehicleNumberPlateText(NetToVeh(netveh),"AVIAO")
+		Citizen.InvokeNative(0xAD738C3085FE7E11,NetToVeh(netveh),true,true)
+		SetVehicleHasBeenOwnedByPlayer(NetToVeh(netveh),true)
+		SetVehicleNeedsToBeHotwired(NetToVeh(netveh),false)
+		SetModelAsNoLongerNeeded(mhash)
+	end
+end
+
+function getVehicleInDirection(coordsfrom,coordsto)
+	local handle = CastRayPointToPoint(coordsfrom.x,coordsfrom.y,coordsfrom.z,coordsto.x,coordsto.y,coordsto.z,10,PlayerPedId(),false)
+	local a,b,c,d,vehicle = GetRaycastResult(handle)
+	return vehicle
+end
---
+++ resources/[jobs]/jobszirix/emp_farmer-delivery/hansolo/hansolo.lua
@@ -0,0 +1,146 @@
+local Tunnel = module("vrp","lib/Tunnel")
+local Proxy = module("vrp","lib/Proxy")
+vRP = Proxy.getInterface("vRP")
+
+--[ CONNECTION ]----------------------------------------------------------------------------------------------------------------
+
+empfarmer = Tunnel.getInterface("vrp_farmer-delivery")
+
+--[ VARIABLES ]-----------------------------------------------------------------------------------------------------------------
+
+local check = 0
+local blips = false
+local working = false
+local hour = 0
+
+--[ FUNCTION ]------------------------------------------------------------------------------------------------------------------
+
+function CalculateTimeToDisplay3()
+	hour = GetClockHours()
+	if hour <= 9 then
+		hour = "0" .. hour
+	end
+end
+
+--[ THREAD ]--------------------------------------------------------------------------------------------------------------------
+
+Citizen.CreateThread(function()
+	while true do
+		local idle = 1000
+		local ped = PlayerPedId()
+		if not IsPedInAnyVehicle(ped) then
+			local x,y,z = table.unpack(GetEntityCoords(ped))
+			local bowz,cdz = GetGroundZFor_3dCoord(configfarmerd.ini.x,configfarmerd.ini.y,configfarmerd.ini.z)
+			local distance = Vdist(configfarmerd.ini.x,configfarmerd.ini.y,configfarmerd.ini.z,x,y,z)
+			local lastVehicle = GetEntityModel(GetPlayersLastVehicle())
+			if distance < 15.1 then
+				idle = 5
+				DrawMarker(23,configfarmerd.ini.x,configfarmerd.ini.y,configfarmerd.ini.z-0.97,0,0,0,0,0,0,1.0,1.0,0.5,136, 96, 240, 180,0,0,0,0)
+				if distance <= 1.2 then
+					drawTexts("PRESSIONE  ~b~E~w~  PARA INICIAR ENTREGAS",4,0.5,0.92,0.35,255,255,255,180)
+					if IsControlJustPressed(1,38) then
+						if lastVehicle == configfarmerd.lastve and empfarmer.checkPlate(lastVehicle) then
+							if empfarmer.checkCrimeRecord() then
+								if not working then
+									CalculateTimeToDisplay3()
+									if parseInt(hour) >= 06 and parseInt(hour) <= 20 then
+										working = true
+										check = math.random(#configfarmerd.deliverys)
+										makeBlipsServices2()
+										TriggerEvent("Notify","sucesso","Entregas iniciada com <b>sucesso</b>.",8000)
+									else
+										TriggerEvent("Notify","importante","Funcionamento é das <b>06:00</b> as <b>20:00</b>.",8000)
+									end
+								end
+							else
+								TriggerEvent("Notify","importante","Sua ficha criminal se encontra suja.",8000)
+							end
+						else
+							TriggerEvent("Notify","importante","Veiculo inválido.",8000)
+						end
+					end
+				end
+			end
+		end
+		Citizen.Wait(idle)
+	end
+end)
+
+--[ THREAD ]--------------------------------------------------------------------------------------------------------------------
+
+Citizen.CreateThread(function()
+	while true do
+		local idle = 1000
+		local ped = PlayerPedId()
+		if working then
+			if not IsPedInAnyVehicle(ped) then
+				local x,y,z = table.unpack(GetEntityCoords(ped))
+				local distance = Vdist(configfarmerd.deliverys[check][1],configfarmerd.deliverys[check][2],configfarmerd.deliverys[check][3],x,y,z)
+				local lastVehicle = GetEntityModel(GetPlayersLastVehicle())local lastVehicle = GetEntityModel(GetPlayersLastVehicle())
+				if distance < 15.1 then
+					idle = 5
+					DrawMarker(21,configfarmerd.deliverys[check][1],configfarmerd.deliverys[check][2],configfarmerd.deliverys[check][3]-0.6,0,0,0,0.0,0,0,0.5,0.5,0.4,136, 96, 240, 180,0,0,0,1)
+					if distance <= 1.2 then
+						drawTexts("PRESSIONE  ~b~E~w~  PARA ENTREGAR GRAOS",4,0.5,0.93,0.50,255,255,255,180)
+						if IsControlJustPressed(1,38) and lastVehicle == 1026149675 and empfarmer.checkPlate(lastVehicle) and empfarmer.checkCrimeRecord() then
+							
+							CalculateTimeToDisplay3()
+							if parseInt(hour) >= 06 and parseInt(hour) <= 20 then
+								if empfarmer.startPayments() then
+									RemoveBlip(blips)
+									check = math.random(#configfarmerd.deliverys)
+									makeBlipsServices2()
+								end
+							else
+								TriggerEvent("Notify","importante","Funcionamento é das <b>06:00</b> as <b>20:00</b>.",8000)
+							end
+						end
+					end
+				end
+			end
+		end
+		Citizen.Wait(idle)
+	end
+end)
+
+--[ CANCEL | THREAD ]--------------------------------------------------------------------------------------------------------------------
+
+Citizen.CreateThread(function()
+	while true do
+		local idle=1000
+		if working then
+			idle=5
+			if IsControlJustPressed(0,168) then
+					working = false
+					RemoveBlip(blips)
+					TriggerEvent("Notify","importante","Você saiu de serviço.",8000)
+			end
+		end
+		Citizen.Wait(idle)
+	end
+end)
+
+--[ FUNCTION ]------------------------------------------------------------------------------------------------------------------
+
+function drawTexts(text,font,x,y,scale,r,g,b,a)
+	SetTextFont(font)
+	SetTextScale(scale,scale)
+	SetTextColour(r,g,b,a)
+	SetTextOutline()
+	SetTextCentre(1)
+	SetTextEntry("STRING")
+	AddTextComponentString(text)
+	DrawText(x,y)
+end
+
+function makeBlipsServices2()
+	blips = AddBlipForCoord(configfarmerd.deliverys[check][1],configfarmerd.deliverys[check][2],configfarmerd.deliverys[check][3])
+	SetBlipSprite(blips,1)
+	SetBlipColour(blips,27)
+	SetBlipScale(blips,0.4)
+	SetBlipAsShortRange(blips,false)
+	SetBlipRoute(blips,true)
+	BeginTextCommandSetBlipName("STRING")
+	AddTextComponentString("Entregas")
+	EndTextCommandSetBlipName(blips)
+end
