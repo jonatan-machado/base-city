8bf2a031aa7fd287de6217dee5adff1e7726da7e
Jonatan Machado <jonatanmachado35@gmail.com>
2022-05-15 17:09:06 +0000

    first commit

---
+++ resources/[others]/lscustoms_items/hansolo/hansolo.lua
@@ -0,0 +1,80 @@
+local Tunnel = module("vrp","lib/Tunnel")
+local Proxy = module("vrp","lib/Proxy")
+vRP = Proxy.getInterface("vRP")
+src = {}
+Tunnel.bindInterface("lscustoms_items",src)
+vSERVER = Tunnel.getInterface("lscustoms_items")
+
+local menuactive = false
+function ToggleActionMenu()
+	menuactive = not menuactive
+	if menuactive then
+		SetNuiFocus(true,true)
+		TransitionToBlurred(1000)
+		SendNUIMessage({ showmenu = true })
+	else
+		SetNuiFocus(false)
+		TransitionFromBlurred(1000)
+		SendNUIMessage({ hidemenu = true })
+	end
+end
+
+RegisterNUICallback("ButtonClick",function(data,cb)
+	if data == "pegar-repairkit" then
+		TriggerServerEvent("mecanic-pegar","repairkit")
+	
+	elseif data == "fechar" then
+		ToggleActionMenu()
+	
+	end
+end)
+
+local lojas = {
+	{ ['x'] = -345.56, ['y'] = -131.38, ['z'] = 39.01 },
+}
+
+Citizen.CreateThread(function()
+	SetNuiFocus(false,false)
+	while true do
+		local idle = 1000
+		for k,v in pairs(lojas) do
+			local ped = PlayerPedId()
+			local x,y,z = table.unpack(GetEntityCoords(ped))
+			local bowz,cdz = GetGroundZFor_3dCoord(v.x,v.y,v.z)
+			local distance = GetDistanceBetweenCoords(v.x,v.y,cdz,x,y,z,true)
+			local lojas = lojas[k]
+			if GetDistanceBetweenCoords(GetEntityCoords(PlayerPedId()), lojas.x, lojas.y, lojas.z, true ) < 1.2 then
+				DrawText3D(lojas.x, lojas.y, lojas.z, "Pressione [~y~E~w~] para acessar a ~y~ESTOQUE~w~.")
+			end
+			if distance <= 10 then
+				DrawMarker(23, lojas.x, lojas.y, lojas.z-0.97,0,0,0,0,0,0,0.7,0.7,0.5, 66, 236, 134, 150,0,0,0,0)
+				idle = 5
+				if distance <= 1.2 then
+					if IsControlJustPressed(0,38) then
+						if vSERVER.checkPermission("mecanico.permissao") then
+							ToggleActionMenu()
+						else
+							TriggerEvent("Notify","negado","Você não é um mecanico")
+						end
+					end
+				end
+			end
+		end
+		Citizen.Wait(idle)
+	end
+end)
+
+function DrawText3D(x,y,z, text)
+    local onScreen,_x,_y=World3dToScreen2d(x,y,z)
+    local px,py,pz=table.unpack(GetGameplayCamCoords())
+    SetTextScale(0.28, 0.28)
+    SetTextFont(4)
+    SetTextProportional(1)
+    SetTextColour(255, 255, 255, 215)
+    SetTextEntry("STRING")
+    SetTextCentre(1)
+    AddTextComponentString(text)
+    DrawText(_x,_y)
+    local factor = (string.len(text)) / 370
+    DrawRect(_x,_y+0.0125, 0.005+ factor, 0.03, 41, 11, 41, 68)
+end
---
+++ resources/[others]/lscustoms_items/skywalker.lua
@@ -0,0 +1,43 @@
+local Tunnel = module('vrp','lib/Tunnel')
+local Proxy = module('vrp','lib/Proxy')
+local Tools = module("vrp","lib/Tools")
+vRP = Proxy.getInterface('vRP')
+vRPclient = Tunnel.getInterface('vRP')
+
+src = {}
+Tunnel.bindInterface('lscustoms_items',src)
+vCLIENT = Tunnel.getInterface('lscustoms_items')
+
+local valores = {
+	{ item = "repairkit", quantidade = 1, compra = 0 },
+}
+
+function src.checkPermission(permission)
+	local src = source
+    local user_id = vRP.getUserId(src)
+    if user_id then
+        return vRP.hasPermission(user_id,permission)
+    end
+end
+
+RegisterServerEvent("mecanic-pegar")
+AddEventHandler("mecanic-pegar",function(item)
+	local source = source
+	local user_id = vRP.getUserId(source)
+	if user_id then
+		for k,v in pairs(valores) do
+			if item == v.item then
+				if vRP.getInventoryWeight(user_id)+vRP.getItemWeight(v.item)*v.quantidade <= vRP.getInventoryMaxWeight(user_id) then
+					if vRP.tryPayment(user_id,parseInt(v.compra)) then
+						vRP.giveInventoryItem(user_id,v.item,parseInt(v.quantidade))
+						TriggerClientEvent("Notify",source,"sucesso","Pegou <b>"..parseInt(v.quantidade).."x "..vRP.itemNameList(v.item)..".")
+					else
+						TriggerClientEvent("Notify",source,"negado","Dinheiro insuficiente.")
+					end
+				else
+					TriggerClientEvent("Notify",source,"negado","Espaço insuficiente.")
+				end
+			end
+		end
+	end
+end)
